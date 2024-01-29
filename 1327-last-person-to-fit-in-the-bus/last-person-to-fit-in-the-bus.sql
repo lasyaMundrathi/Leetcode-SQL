68cc7541706fac0b431b147a3475197d4d34ed0f
lasyaMundrathi <98383338+lasyaMundrathi@users.noreply.github.com>
2024-01-29 12:19:14 +0000

    Time: 1605 ms (60.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,7 @@
+select person_name from 
+(
+    select person_name, weight, turn,sum(weight) over(order by turn) as Total_Weight from Queue
+) tmp
+where Total_Weight<=1000
+order by turn desc
+limit 1;
