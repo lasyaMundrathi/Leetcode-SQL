ceda214330c88f48e2e38566811aa5ece25666d4
lasyaMundrathi <98383338+lasyaMundrathi@users.noreply.github.com>
2024-01-31 15:42:42 +0000

    Time: 666 ms (59.40%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,17 @@
+SELECT 
+    distinct visited_on,
+    amount,
+    ROUND(amount/7, 2) AS average_amount
+FROM 
+    (SELECT
+        visited_on,
+        SUM(amount) OVER (
+            ORDER BY visited_on 
+            RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+        ) AS amount,
+
+        MIN(visited_on) OVER() as firstDay
+    FROM Customer
+    ) AS c
+WHERE
+    visited_on >= firstDay + INTERVAL 6 DAY;
